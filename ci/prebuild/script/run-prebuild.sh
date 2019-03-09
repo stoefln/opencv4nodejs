b18d9c0ee0bb39a74f67faf45da6f11b42a80843
Simon Hofmann <simon.hofmann@consol.de>
2019-03-09 04:18:41 +0100

    Updated build config

--- ci/prebuild/script/run-prebuild.sh
+++ ci/prebuild/script/run-prebuild.sh
@@ -3,6 +3,4 @@ export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
 echo "Installing dependencies" &&\
 npm install --unsafe-perm &&\
 echo "Running prebuild" &&\
-cd ./prebuild &&\
-npm install --unsafe-perm &&\
 npm run prebuild
