ae5b2ce6447d77c0a94b8ad74eeeef27641cca64
Simon Hofmann <simon.hofmann@consol.de>
2019-03-09 05:06:07 +0100

    Reverted build

--- ci/prebuild/script/run-prebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export OPENCV4NODEJS_DISABLE_AUTOBUILD=1
-echo "Installing dependencies" &&\
-npm install --unsafe-perm &&\
-echo "Running prebuild" &&\
-npm run prebuild -- -u $GITHUB_TOKEN
