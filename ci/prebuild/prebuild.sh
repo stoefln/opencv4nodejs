1d8e6a95aeb1327994388e10c242ba8984154f09
Simon Hofmann <simon.hofmann@consol.de>
2019-03-09 04:46:25 +0100

    Fixed docker run

--- ci/prebuild/prebuild.sh
+++ ci/prebuild/prebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 image=opencv4nodejs-ci_$1
 docker build -t $image -f ./Dockerfile --build-arg TAG=$1 ../../
-docker run $image -e GITHUB_TOKEN=$GITHUB_TOKEN
+docker run -e GITHUB_TOKEN=$GITHUB_TOKEN $image
