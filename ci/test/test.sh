32d2d8ffa3b5f097ccceb88ebaea0f3a680156bd
Simon Hofmann <simon.hofmann@consol.de>
2019-05-16 23:33:39 +0200

    Restructured ci jobs

--- ci/test/test.sh
+++ ci/test/test.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
-if [[ "$TRAVIS_OS_NAME" == "linux" ]];
-then
-    image=opencv4nodejs-ci_$1
-    docker build -t $image -f ./Dockerfile --build-arg TAG=$1 ../../
-    docker run $image
-elif [[ "$TRAVIS_OS_NAME" == "osx" ]];
-then
-    cd ../../ &&\
-    npm install &&\
-    cd test &&\
-    npm install &&\
-    npm test &&\
-    npm run test-externalMemTracking
-fi
+cd ../../ &&\
+npm install &&\
+cd test &&\
+npm install &&\
+npm test &&\
+npm run test-externalMemTracking
