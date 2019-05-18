ae01cf6a03d9a1cd4761aafb730f6fed388a18af
Simon Hofmann <simon.hofmann@consol.de>
2019-05-18 15:08:26 +0200

    Updated build scripts

--- ci/prebuild/prebuild.sh
+++ ci/prebuild/prebuild.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-if [[ "$TRAVIS_OS_NAME" == "linux" ]];
+if [[ "$TRAVIS_OS_NAME" == "linux" ]] || [[ "$TRAVIS_OS_NAME" == "osx" ]];
 then
-    unset OPENCV4NODEJS_DISABLE_AUTOBUILD
+    dirs=(
+    /opt/opencv-prebuilt
+    /usr/local/opt/opencv-prebuilt
+    )
+
+    for dir in ${dirs[*]}; do if [ -d $dir ]; then rm -rf $dir; fi; done
 fi
+if [[ "$TRAVIS_OS_NAME" == "windows" ]];
+then
+    for dir in ${dirs[*]}; do if [ -d $dir ]; then rm -rf $dir; fi; done
+fi
+
 cd ../../ &&\
 npm install --unsafe-perm &&\
 npm test
